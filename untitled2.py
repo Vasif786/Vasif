e303f7b7ed88c0c0c3b988d18695e8b03e06bf14
Vasif786 <farjanabanurajpura@gmail.com>
2026-03-11 18:03:16 +0530

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,36 @@
+print("Coffee Menu:")
+print("1. Espresso")
+print("2. Cappuccino")
+print("3. Latte")
+Coffee = {"1","2","3"}
+
+
+
+Coffee2 = print(input("enter the choice by entering number:"))
+Coffee3 = print(input("enter the quantity:"))
+if Coffee2 == 1 :
+    print("Thank you for selecting Espresso.")
+elif Coffee2 == 2 :
+    print("Thank you for selecting Cappuccino.")
+elif Coffee2 == 3 :
+    print("Thank you for selecting Latte.")
+
+Coffee4 = print(input("enter the size their drink ,(200ml or 400ml):"))
+
+if Coffee2 == 1 :
+    print("you choose Espresso.")
+elif Coffee2 == 2 :
+    print("you choose Cappuccino.")
+elif Coffee2 == 3 :
+    print("you choose Latte.")
+
+
+
+a = ("Espresso : 200ml - 100Rs, 400ml - 200Rs")
+b = ("Cappuccino : 200ml - 150Rs, 400ml - 250Rs")
+c = ("Latte : 200ml - 200Rs, 400ml - 300Rs")
+print("their quantity is",Coffee3)
+print("their size is",Coffee4)
+print("Espresso : 200ml - 100Rs, 400 - 200Rs")
+print("Cappuccino : 200ml - 150Rs, 400 - 250Rs")
+print("Latte : 200ml - 200Rs, 400 - 300Rs")
